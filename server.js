6ebeecc75f025174acd54718a7f96c1137d8a99d
Akash Kumar Singh <akashkumarsingh3@gmail.com>
2021-04-25 15:57:12 +0530

    database uri changed

--- server.js
+++ server.js
@@ -1,37 +1,40 @@
-var express=require('express');
-var path= require('path');
-var mongoose=require('mongoose');
+var express = require("express");
+var path = require("path");
+var mongoose = require("mongoose");
 
-var bodyparser=require('body-parser');
-var cors=require('cors');
-var UserRoutes=require('./backend/routes/user');
-var listRoutes = require('./backend/routes/task_list');
-var archiveRoutes = require('./backend/routes/archive');
-var app=express();
+var bodyparser = require("body-parser");
+var cors = require("cors");
+var UserRoutes = require("./backend/routes/user");
+var listRoutes = require("./backend/routes/task_list");
+var archiveRoutes = require("./backend/routes/archive");
+var app = express();
 //app.use(cors());
-app.use(bodyparser.urlencoded({extended:false}));
-app.use(bodyparser.json());
-app.use(express.static(path.join(__dirname, 'dist/Task-Management')));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "dist/Task-Management")));
 
-//Send all other requests to the Angular app 
-app.get('/', (req, res)=> {
-    console.log(req);
-    res.sendFile(path.join(__dirname, 'dist/Task-Management/index.html'),(err,resp)=>{
-      if(err)console.log("err"+err);
+//Send all other requests to the Angular app
+app.get("/", (req, res) => {
+  console.log(req);
+  res.sendFile(
+    path.join(__dirname, "dist/Task-Management/index.html"),
+    (err, resp) => {
+      if (err) console.log("err" + err);
       console.log(resp);
-    });
+    }
+  );
 });
-mongoose.connect('mongodb://root:Abc123@ds229835.mlab.com:29835/task_management',{
-    useCreateIndex: true,
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  });
+const url =
+  "mongodb+srv://test123:test123@cluster0.snlcs.mongodb.net/TaskManagement?retryWrites=true&w=majority";
+mongoose.connect(url, {
+  useCreateIndex: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-app.use('/api',UserRoutes);
-app.use('/task',listRoutes);
-app.use('/archive',archiveRoutes);
-const port=process.env.PORT || 3000;
-app.listen(port,(req,res)=>
-{
-console.log(`Running in port ${port}`);
+app.use("/api", UserRoutes);
+app.use("/task", listRoutes);
+app.use("/archive", archiveRoutes);
+const port = process.env.PORT || 3000;
+app.listen(port, (req, res) => {
+  console.log(`Running in port ${port}`);
 });
